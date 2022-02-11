93b03e4be808bcf56f26328e12f849fa34599265
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2022-02-11 12:15:59 +0530

    Update linux-download-qpdf.bash

--- build-scripts/linux-download-qpdf.bash
+++ build-scripts/linux-download-qpdf.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-QPDF_RELEASE=https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.4.0/qpdf-10.4.0.tar.gz
-#${QPDF_PATTERN//VERSION/$1}
+QPDF_RELEASE=${QPDF_PATTERN//VERSION/$1}
+#https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.4.0/qpdf-10.4.0.tar.gz
 
 mkdir qpdf && wget -q $QPDF_RELEASE -O - | tar xz -C qpdf --strip-components=1
